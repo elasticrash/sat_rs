06b51d4c059261debc682871d0be0db1c202e364
Stefanos Kouroupis <stefanos.kouroupis@cloudcall.com>
2019-10-11 14:14:50 +0100

    properties on clause trait

--- src/models/clause.rs
+++ src/models/clause.rs
@@ -3,4 +3,11 @@ use crate::models::lit::Lit;
 pub struct Clause {
     pub data: Vec<Lit>,
     is_learnt: bool,
+    activity: f32
+}
+
+pub trait IClause {
+    fn new() -> Self;
+    fn size() -> i32;
+    fn learnt() -> bool;
 }
