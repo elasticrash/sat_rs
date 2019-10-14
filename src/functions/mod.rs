53806844a9f25742bd053be8da7ffb8b250612c5
Stefanos Kouroupis <stefanos.kouroupis@cloudcall.com>
2019-10-14 08:22:27 +0100

    add functions to module

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -1,4 +1,9 @@
+pub mod analyse;
 pub mod analyse_final;
 pub mod enqueue;
-pub mod search;
 pub mod new_clause;
+pub mod propagate;
+pub mod reduce_db;
+pub mod search;
+pub mod simplify_db;
+pub mod solve;
