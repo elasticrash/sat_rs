b565b5a8714e46d0eb9b9756ab4e5e2ee582534d
Stefanos Kouroupis <stefanos.kouroupis@cloudcall.com>
2019-10-14 11:08:07 +0100

    placeholder functions on new clause

--- src/functions/new_clause.rs
+++ src/functions/new_clause.rs
@@ -3,3 +3,21 @@ use crate::models::lit::Lit;
 pub fn new_clause(_ps: Vec<Lit>, _learnt:bool){
     
 }
+
+pub fn basic_clause_simplification(_ps:Vec<Lit>, _copy: bool) -> Vec<Lit>{
+    let qs = Vec::new();
+    return qs;
+}
+
+pub fn reorder_by_level(_ps: Vec<Lit>){
+
+}
+
+fn new_clause_pr(_ps: Vec<Lit>, _learnt:bool, _theory_clause:bool, _copy:bool){
+
+}
+
+pub fn remove(){}
+pub fn simplify(){}
+pub fn remove_watch(){}
+pub fn new_var(){}
