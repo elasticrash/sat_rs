aae373b798229b76eb973983df5eeec4cf7cc44f
stefano <s.kouroupis@gmail.com>
2019-10-27 00:59:01 +0100

    analyse_final

--- src/functions/analyse_final.rs
+++ src/functions/analyse_final.rs
@@ -1,5 +1,6 @@
 use crate::models::clause::*;
 use crate::models::lbool::*;
+use crate::models::lit::*;
 use crate::models::solverstate::*;
 
 /*_________________________________________________________________________________________________
@@ -19,5 +20,43 @@ pub fn analyse_final(_confl: Clause, _skip_first: bool, solver_state: &mut Solve
         return;
     }
 
-    let seen: Vec<Lbool> = Vec::new();
+    let mut seen: Vec<Lbool> = solver_state.analyze_seen.clone();
+    let mut istart: i32 = 0;
+    if _skip_first {
+        istart = 1
+    } else {
+        istart = 0
+    };
+    for y in istart.._confl.data.len() as i32 {
+        let x: usize = var(&_confl.data[y as usize]) as usize;
+        if solver_state.level[x] > 0 {
+            seen[x] = Lbool::True;
+        }
+    }
+
+    let mut end: i32 = solver_state.trail_lim[solver_state.root_level as usize];
+    if solver_state.root_level >= solver_state.trail_lim.len() as i32 {
+        end = (solver_state.trail.len() - 1) as i32;
+    }
+
+    for y in (end..solver_state.trail_lim[0] + 1).rev() {
+        let x: usize = var(&solver_state.trail[y as usize]) as usize;
+
+        if seen[x] != Lbool::True {
+            match solver_state.reason[x].clone() {
+                Some(clause) => {
+                    for j in 1..clause.data.len() {
+                        if solver_state.level[var(&clause.data[j as usize]) as usize] > 0 {
+                            seen[var(&clause.data[j as usize]) as usize] = Lbool::True;
+                        }
+                    }
+                }
+                None => {
+                    solver_state.conflict.push(!solver_state.trail[y as usize]);
+                }
+            }
+
+            seen[x] = Lbool::Undef0;
+        }
+    }
 }
