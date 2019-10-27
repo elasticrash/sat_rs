4056e4d95ba6d460e85eeea4d4500856f268d265
stefano <s.kouroupis@gmail.com>
2019-10-27 22:59:28 +0000

    propagate

--- src/functions/propagate.rs
+++ src/functions/propagate.rs
@@ -1,5 +1,6 @@
-use crate::models::solverstate::*;
 use crate::models::clause::*;
+use crate::models::lit::*;
+use crate::models::solverstate::*;
 
 /*_________________________________________________________________________________________________
 |
@@ -16,7 +17,15 @@ use crate::models::clause::*;
 pub fn propagate(solver_state: &mut SolverState) {
     let confl: Clause;
 
-    while(solver_state.qhead < solver_state.trail.len() as i32){
-
+    while (solver_state.qhead < solver_state.trail.len() as i32) {
+        solver_state.solver_stats.propagations += 1.0;
+        solver_state.simp_db_props -= 1.0;
+        let p: Lit = solver_state.trail[solver_state.qhead as usize];
+        solver_state.qhead += 1;
+        let ws: Vec<Clause> = solver_state.watches[index(p.clone()) as usize].clone();
+        let mut i: i32 = 0;
+        let mut j: i32 = 0;
+        let mut end = i + ws.len() as i32;
+        while (i != end) {}
     }
 }
